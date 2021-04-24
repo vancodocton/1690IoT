be95fbde793c9a7afc665878659c0cf750e38e4a
vancodocton <duongtruong.nguyenky@gmail.com>
2021-04-25 00:09:27 +0700

    update Lock.State after changing state

--- SecureLock.ino
+++ SecureLock.ino
@@ -6,20 +6,24 @@
 SecureLock::SecureLock()
 {
     state = Locked;
+    Serial.println("The lock is initialized");
 }
 bool SecureLock::Lock()
 {
     Serial.println("Locking");
+    state = Locked;
     return true;
 }
 bool SecureLock::Unlock()
 {
     Serial.println("Unlocking");
+    state = Unlocked;
     return true;
 }
 bool SecureLock::Authenticate()
 {
-    Serial.println("Authorizing");
+    Serial.println("Authenticating");
+    state = Authenticated;
     return true;
 }
 int SecureLock::State()
