238a68315c87ce086e8db9f6309151f0857e51b8
vancodocton <duongtruong.nguyenky@gmail.com>
2021-04-25 00:31:16 +0700

    add the physical lock "Servo" function

--- SecureLock.ino
+++ SecureLock.ino
@@ -3,20 +3,27 @@
 #include "SecureLock.h"
 #include <Blynk.h>
 
+
+
 SecureLock::SecureLock()
 {
     state = Locked;
+    servo = Servo();
+    servo.attach(D0);
+    
     Serial.println("The lock is initialized");
 }
 bool SecureLock::Lock()
 {
     Serial.println("Locking");
+    servo.write(0);
     state = Locked;
     return true;
 }
 bool SecureLock::Unlock()
 {
     Serial.println("Unlocking");
+    servo.write(90);
     state = Unlocked;
     return true;
 }
