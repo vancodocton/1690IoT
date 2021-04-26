b8ea786fe1fe3a8722bf8904e1dd0856e91ecb20
vancodocton <duongtruong.nguyenky@gmail.com>
2021-04-26 08:08:49 +0700

    add header of class View

--- View.h
+++ View.h
@@ -1,4 +1,19 @@
 #ifndef View_h
 #define View_h
 
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+#include <BlynkSimpleEsp8266.h>
+
+class View
+{
+public:
+    View();
+    void Message(String message);
+    void MessageLCD(String message);
+    LiquidCrystal_I2C lcd = LiquidCrystal_I2C(0x27, 16, 2);
+
+private:
+    WidgetTerminal terminal = WidgetTerminal(V0);
+};
 #endif
