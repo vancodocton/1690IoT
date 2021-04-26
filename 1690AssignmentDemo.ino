444872a15ebf64295e626626a2c93ebd88d4837f
vancodocton <duongtruong.nguyenky@gmail.com>
2021-04-26 08:14:56 +0700

    update main ino

--- 1690AssignmentDemo.ino
+++ 1690AssignmentDemo.ino
@@ -3,28 +3,16 @@
 #include <ESP8266WiFi.h>
 #include <BlynkSimpleEsp8266.h>
 
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
 #include "SecureLock.h"
+#include "View.h"
 
 //char auth[] = "TwCg6uFlHUNnz-yRJLxW-tfUWfP9Bclc";
 char auth[] = "iE7NulbKfx6ZgO4gBgAS4T5ZYjGcA1Ot";
 char ssid[] = "HostpotTP";
 char pass[] = "12345678";
-
-class View
-{
-public:
-	View()
-	{
-	}
-	void Message(String message)
-	{
-		Serial.println(message);
-		terminal.println(message);
-	}
-
-private:
-	WidgetTerminal terminal = WidgetTerminal(V0);
-};
 
 SecureLock lock;
 View view;
@@ -34,10 +22,16 @@ WidgetTerminal terminal(V0);
 void setup()
 {
 	Serial.begin(9600);
+
 	Blynk.begin(auth, ssid, pass, IPAddress(192, 168, 137, 1), 8080);
 
 	lock = SecureLock();
 	view = View();
+
+	terminal.clear();
+	view.Message("Starting... Done!");
+
+	Blynk.virtualWrite(V1, 0);
 }
 
 void loop()
@@ -53,50 +47,68 @@ BLYNK_WRITE(V1)
 	// If button is pushed
 	if (btnState == 1)
 	{
-		// If button is locked
+		switch (lock.State())
+		{
+		case Locked:
+			break;
+		case Unlocked:
+			break;
+		case Authenticated:
+			break;
+		}
 		if (lock.State() == Locked)
 		{
 			// If authorizing sucess
 			if (lock.Authenticate() == true)
 			{
-				view.Message("Authenticated successfully!");
+				view.Message("Authenticated successful!");
+				view.MessageLCD("Authenticated!");
 				// Unlocking
-				if (lock.Lock() == true)
+				if (lock.Unlock() == true)
 				{
-					view.Message("Unlooking is successful! The lock is unlocked.");
+					view.Message("The lock is unlocked.");
+					view.MessageLCD("The lock is unlocked.");
 				}
 				else
 				{
-					view.Message("Unlooking is failed because of unknown reason.");
+					view.Message("Unlooking is failed");
+					view.Message("Problems: Unknown");
 				}
 			}
 			else
 			{
-				view.Message("Authenticated failed! Cannot unlocking the lock.");
+				view.Message("Authenticated failed!");
+				view.Message("Cannot unlocking the lock.");
 			}
 		}
-		if (lock.State() == Unlocked)
+		else if (lock.State() == Unlocked)
 		{
-			if (lock.Unlock() == true)
+			if (lock.Lock() == true)
 			{
-				view.Message("Locking is successful! The lock is locked.");
+				view.Message("Locking is successful!");
+				view.Message("The lock is locked.");
 			}
 		}
 		Blynk.virtualWrite(V1, 0);
 	}
-	else
-		switch (lock.State())
-		{
-		case Locked:
-			Blynk.setProperty(V1, "offLable", "Unlock");
-			Blynk.setProperty(V1, "onLable", "Unlocking");
-			break;
-		case Unlocked:
-			Blynk.setProperty(V1, "offLable", "Lock");
-			Blynk.setProperty(V1, "onLable", "Locking");
-			break;
-		default:
-			Blynk.setProperty(V1, "offLable", "NaN");
-			Blynk.setProperty(V1, "onLable", "NaN");
-		}
+	switch (lock.State())
+	{
+	case Locked:
+		Blynk.setProperty(V1, "offLabel", "Unlock");
+		Blynk.setProperty(V1, "onLabel", "Unlocking");
+		Blynk.setProperty(V1, "onBackColor", "#FFFF00");
+		Blynk.setProperty(V1, "offBackColor", "#FF0000");
+		break;
+	case Unlocked:
+		Blynk.setProperty(V1, "offLabel", "Lock");
+		Blynk.setProperty(V1, "onLabel", "Locking");
+		Blynk.setProperty(V1, "onBackColor", "#FFFF00");
+		Blynk.setProperty(V1, "offBackColor", "#00FF00");
+		break;
+	default:
+		Blynk.setProperty(V1, "offLabel", "NaN");
+		Blynk.setProperty(V1, "onLabel", "NaN");
+		Blynk.setProperty(V1, "onBackColor", "#FFFF00");
+		Blynk.setProperty(V1, "offBackColor", "#808080");
+	}
 }
